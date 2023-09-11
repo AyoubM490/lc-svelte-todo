523e9132adfa8bd47898bd8f62f8b480a17e5728
Ayoub CHAHIR <ayoub.chahir@newcogroup.com>
2023-09-11 16:44:58 +0100

    Episode 5 - Component Communication using Events

---
+++ src/TodoForm.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+    let todoTitle = "";
+    function addTodo() {
+        dispatch("todoAdded", {
+            todoTitle,
+        });
+        todoTitle = "";
+    }
+</script>
+
+<form action="#" on:submit|preventDefault={addTodo}>
+    <input
+            type="text"
+            class="todo-input"
+            placeholder="What do you need to do?"
+            bind:value={todoTitle}
+    />
+</form>
---
+++ src/TodoFormMiddle.svelte
@@ -0,0 +1,14 @@
+<script>
+    import TodoForm from "./TodoForm.svelte";
+    // import {createEventDispatcher} from "svelte";
+    //
+    // const dispatch = createEventDispatcher();
+    //
+    // function addTodo(event) {
+    //     dispatch("todoAdded", {
+    //         todoTitle: event.detail.todoTitle,
+    //     });
+    // }
+</script>
+
+<TodoForm on:todoAdded />
